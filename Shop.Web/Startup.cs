177637b1641581071d8685b4f3e9f04abad30662
Sergio Serrano Vargas <ssergio0@hotmail.com>
2019-09-20 10:37:52 -0600

    Agregar SeedDb

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -27,6 +27,9 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddTransient<SeedDb>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
